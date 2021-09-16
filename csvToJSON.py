42a8ebb440b9910085ae5af7374fa0f3b1eeb85a
Brian Kozub <ticet11@gmail.com>
2021-09-15 23:44:36 -0600

    Combined ip addresses and zip data

--- csvToJSON.py
+++ csvToJSON.py
@@ -1,32 +1,30 @@
 import json
 import csv
+import os
 
-with open("uszips.csv", "r") as f:
-    reader = csv.reader(f)
-    next(reader)
-    data = {"texaszips": {}}
-    for row in reader:
-        if row[4] == 'TX':
-            data["texaszips"][row[0]] = {
-                "latitude": row[1],
-                "longitude": row[2],
-                "city": row[3],
-                "stateid": row[4],
-                "statename": row[5],
-                "zcta": row[6],
-                "parent_zcta": row[7],
-                "population": row[8],
-                "density": row[9],
-                "countyfips": row[10],
-                "countyname": row[11],
-                "countyweights": row[12],
-                "countynamesall": row[13],
-                "countyfipsall": row[14],
-                "imprecise": row[15],
-                "military": row[16],
-                "timezone": row[17],
+with open(os.path.join(os.path.dirname(__file__),
+                       './ip_addresses.json')) as ip_file:
+    cities = json.load(ip_file)
+    with open(os.path.join(os.path.dirname(__file__),
+                           './txzips.json')) as zip_file:
+        zips = json.load(zip_file)
+        data = {"state": ["TX", "Texas"], "cities": {}}
+        for key in cities["TX"]:
+            data["cities"][key] = {
+                "ip_addresses": cities["TX"][key],
+                "zip_codes": []
             }
 
+            for zipKey in zips["zip_codes"]:
+                if zipKey["city"] == key:
+                    data["cities"][key]["zip_codes"].append({
+                        "zip":
+                        zipKey["zip"],
+                        "countyname":
+                        zipKey["countyname"],
+                        "pop":
+                        zipKey["pop"],
+                    })
 
-with open("txzips.json", 'w') as f:
+with open("tx.json", 'w') as f:
     json.dump(data, f, indent=4)
