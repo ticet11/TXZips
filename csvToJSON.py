37e5c5571a83206c3db83f5a6b11b7e12ba8792e
Brian Kozub <ticet11@gmail.com>
2021-09-18 19:28:28 -0600

    Pulling ipRanges from csv as subnets

--- csvToJSON.py
+++ csvToJSON.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import os
+import ipaddress
 
 with open("IP2LOCATION-LITE-DB9.CSV") as f:
 
@@ -11,17 +12,31 @@ with open("IP2LOCATION-LITE-DB9.CSV") as f:
 
     reader = csv.reader(f)
     next(reader)
-    data = []
+    data = {}
     for row in reader:
         if row[4] == 'Texas':
-            data.append({
-                "ip_address_range": {
-                    "low": long2DotIP(int(row[0])),
-                    "high": long2DotIP(int(row[1])),
-                },
-                "city": row[5],
-                "zip": row[8]
-            })
+            formattedIpRanges = []
+            rowIpRanges = [
+                ipaddr for ipaddr in ipaddress.summarize_address_range(
+                    ipaddress.IPv4Address(int(row[0])),
+                    ipaddress.IPv4Address(int(row[1])))
+            ]
+            for rowIpRange in rowIpRanges:
+                formattedIpRanges.append(format(rowIpRange))
+
+            data[row[8]] = {
+                "ip_address_range": formattedIpRanges,
+            }
+
+    #     if row[4] == 'Texas' and row[5] == 'Abilene':
+    #         rowIpRanges = [
+    #             ipaddr for ipaddr in ipaddress.summarize_address_range(
+    #                 ipaddress.IPv4Address(int(row[0])),
+    #                 ipaddress.IPv4Address(int(row[1])))
+    #         ]
+    #         for rowIpRange in rowIpRanges:
+    #             ipRanges.append(format(rowIpRange))
+    # print(ipRanges)
 
 with open("tx_ip_by_zip.json", 'w') as f:
     json.dump(data, f, indent=4)
